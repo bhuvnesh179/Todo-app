49c86dcd2b7c68f9e8945d3e66850144942ad3b9
bhuvnesh179 <bhuvneshbansal179@gmai.com>
2024-09-03 22:54:30 +0530

    defining crud operation on different end point

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+import { todo } from "./db";
 import { createTodo, updateTodo } from "./type";
 
 app.use(express.json());
 
-app.post("/todo", function(req, res){
+app.post("/todo", async function(req, res){
     const createPayload = req.body;
     const parsePayload = createTodo.safeParse(createPayload);
     if(!parsePayload.success){
@@ -13,21 +14,41 @@ app.post("/todo", function(req, res){
         })
         return;
     }
+   await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false;
+    })
+    res.json({
+        msg: "Todo created"
+    })
 });
 
-app.get("/todos", function(req, res){
+app.get("/todos", async function(req, res){
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
+});
+
+app.put("/completed", async function(req, res){
     const createPayload = req.body;
     const parsePayload = updateTodo.safeParse(createPayload);
-    if(!parsePayload.success){
+    if (!parsePayload.success) {
         res.status(411).json({
             msg: "you send the wrong iputs",
         })
         return;
     }
-});
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed: true
+    })
 
-app.put("/completed", function(req, res){
-
+    res.json({
+        msg: "Todo marked as completed."
+    })
 });
 
 app.listen(3000);
