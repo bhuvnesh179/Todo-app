42056318d34041cbbb538aac2969fa93d362b2bb
bhuvnesh179 <bhuvneshbansal179@gmai.com>
2024-09-04 18:33:51 +0530

    add a fronted of application and make some component

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,13 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="Title"></input>
+        <br></br>
+        <br></br>
+        <input type="text" placeholder="Description"></input>
+        <br></br>
+        <br></br>
+
+        <button>Add a Todo</button>
+
+    </div>
+}
